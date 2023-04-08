46a5a43c0b212fe42d38fbc0c3ee577255dcd1fe
Alma Elphias <aelphias@icloud.com>
2023-04-08 13:28:50 +0300

    initial

---
+++ hello_world_/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hello_world_
+//
+//  Created by aelphias on 08.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello_world_/hello_world_App.swift
@@ -0,0 +1,17 @@
+//
+//  hello_world_App.swift
+//  hello_world_
+//
+//  Created by aelphias on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct hello_world_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
